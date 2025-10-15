3e992604a75ca45df8eb1c4b6c66178f663952f6
Maryam Kiran <maryam.kiran@nestosh.com>
2025-10-15 15:30:30 +0500

    Completed Todo Api with structural approach

---
+++ Todo/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Model;
+using Todo.Service;
+
+namespace Todo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoService _service; 
+
+        public TodoController(TodoService service )
+        {
+            _service = service; 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateTodoDto dto)
+        { 
+
+            var result = await _service.CreateTodoAsync(dto);
+            return CreatedAtAction(nameof(Create), new { id = result.Id }, result);
+        }
+    }
+}
